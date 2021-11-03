a1b6b0cb8c953265bbc46b9b629f875e362e2812
Umarkhabiliy <khabiliyshakhzod@gmail.com>
2021-11-03 15:38:32 +0500

    fetch-data-from-api-by-getx

--- fluttergetx/lib/screens/home_page.dart
+++ fluttergetx/lib/screens/home_page.dart
@@ -1,38 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:fluttergetx/screens/controller/get_controller.dart';
 import 'package:get/get.dart';
 
-class MyHomePage extends StatelessWidget {
+//That's where GetWidget shines...
+// as you can use it, for example,
+// to keep a list of Todo items.
+//So, if the widget gets "rebuilt",
+// it will keep the same controller instance.
+class MyHomePage extends GetWidget<FetchData> {
   MyHomePage({Key? key}) : super(key: key);
-//.obs we add .obs to a variable, 
+//.obs we add .obs to a variable,
 //we basically convert it to a stream of variables.
 // This stream is 'reactive',
 // which means it can change value
-// in reaction to events (like click of a button). 
+// in reaction to events (like click of a button).
   final Rx<bool> _isAdmin = false.obs;
+  final FetchData fetchData = Get.put(FetchData());
+//Get.put makes the dependency available to all the child routes.
+// So, in case we need to access the same instance in some other class,
+// we can do that using Get.find.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("GetX"),
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-//Obx Listens to observable (obs) changes.
-// Controller needs to already be declared/initialized elsewhere to use.
-              Obx(
-                () => Text(
-                  "SetState: $_isAdmin",
-                  style:
-                      const TextStyle(color: Colors.greenAccent, fontSize: 18),
-                ),
-              ),
               ElevatedButton(
-                onPressed: () {
-                  _isAdmin.value = !_isAdmin.value;
+                onPressed: () async {
+                 await fetchData.getPosts();
                 },
-                child: const Text("CLick Me"),
+                child: const Text("Click Me"),
               ),
+             SizedBox(
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: GetBuilder<FetchData>(
+                  builder: (getSnap) {
+                    return ListView.builder(
+                        itemCount: getSnap.postModels.length,
+                        itemBuilder: (ctx, indx) {
+                          return ListTile(
+                            title: Text(getSnap.postModels[indx].title.toString()),
+                          );
+                        });
+                  },
+                ),
+              )
             ],
           ),
         ),
